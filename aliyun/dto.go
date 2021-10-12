17a443457d12a54fcac3e3beb25b9d8da33bd381
keke.li <lisensen@agora.io>
2021-10-12 15:11:44 +0800

    feat: change page of layout

--- aliyun/dto.go
+++ aliyun/dto.go
@@ -2,41 +2,41 @@ package aliyun
 
 // SaveRamUserRequest 具体注释参考: https://help.aliyun.com/document_detail/185726.html?spm=a2c4g.11186623.6.737.207d1f2fRXubtd
 type SaveRamUserRequest struct {
-	UserName string `json:"user_name"`
+	UserName    string `json:"user_name"`
 	DisplayName string `json:"display_name"`
 	MobilePhone string `json:"mobile_phone"`
-	Email string `json:"email"`
-	Comments string `json:"comments"`
+	Email       string `json:"email"`
+	Comments    string `json:"comments"`
 }
 
 type RamUserInfo struct {
-	UserID   string `json:"user_id"`
-	CreateDate string `json:"create_date"`
-	UpdateDate string `json:"update_date"`
+	UserID            string `json:"user_id"`
+	CreateDate        string `json:"create_date"`
+	UpdateDate        string `json:"update_date"`
 	UserPrincipalName string `json:"user_principal_name"`
-	DisplayName string `json:"display_name"`
-	MobilePhone string `json:"mobile_phone"`
-	Email string `json:"email"`
-	Comments string `json:"comments"`
-	LastLoginDate string `json:"last_login_date"`
+	DisplayName       string `json:"display_name"`
+	MobilePhone       string `json:"mobile_phone"`
+	Email             string `json:"email"`
+	Comments          string `json:"comments"`
+	LastLoginDate     string `json:"last_login_date"`
 }
 
 type RamUserResponse struct {
-     RequestID  string `json:"request_id"`
-     User RamUserInfo `json:"user"`
+	RequestID string      `json:"request_id"`
+	User      RamUserInfo `json:"user"`
 }
 
 type GroupInfo struct {
-	DisplayName   string `json:"display_name"`
-	GroupName string `json:"group_name"`
-	GroupID  string `json:"group_id"`
-	Comments string `json:"comments"`
-	JoinDate string `json:"join_date"`
-	UpdateDate string `json:"update_date"`
-	CreateDate string `json:"create_date"`
+	DisplayName string `json:"display_name"`
+	GroupName   string `json:"group_name"`
+	GroupID     string `json:"group_id"`
+	Comments    string `json:"comments"`
+	JoinDate    string `json:"join_date"`
+	UpdateDate  string `json:"update_date"`
+	CreateDate  string `json:"create_date"`
 }
 
 type AddOrRemoveUserToGroupRequest struct {
 	GroupName string `json:"group_name"`
-	UserName string `json:"user_name"`
+	UserName  string `json:"user_name"`
 }
--- aliyun/go.mod
+++ aliyun/go.mod
@@ -3,15 +3,11 @@ module github.com/KeKe-Li/go-component/aliyun
 go 1.17
 
 require (
-    github.com/gotomicro/ego v0.6.14
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/mitchellh/mapstructure v1.3.2 // indirect
-	github.com/opentracing/opentracing-go v1.1.0 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
-	github.com/uber/jaeger-client-go v2.23.1+incompatible // indirect
-	github.com/uber/jaeger-lib v2.4.1+incompatible // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.17.0 // indirect
-	google.golang.org/grpc v1.39.0 // indirect
+	github.com/BurntSushi/toml v0.3.1
+	github.com/alibabacloud-go/darabonba-openapi v0.1.7
+	github.com/alibabacloud-go/ims-20190815/v2 v2.0.1
+	github.com/alibabacloud-go/tea v1.1.15
+	github.com/gotomicro/ego v0.5.6
+	github.com/stretchr/testify v1.6.1
+	go.uber.org/zap v1.15.0
 )
